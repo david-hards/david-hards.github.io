47fd3bfb5ec91168d958fe40cc765005139028c9
David Hards <davidhards@gmail.com>
2024-01-16 22:24:36 +1300

    created responsive navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,27 @@
-function testFunction() {
-    let background = document.getElementById('body')
-    background.style.backgroundColor = 'red';
-    alert('js is working')
+const hamburger = document.querySelector('.nav__hamburger')
+const linksContainer = document.querySelector('.nav__menu')
+const links = document.querySelectorAll('.nav__menu__link')
+
+hamburger.addEventListener('click', ()=> {
+    linksContainer.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
+
+window.addEventListener('resize', ()=> {
+    if (window.matchMedia('(max-width: 550px)').matches){
+        closeMenu()
+    }
+})
+
+if(window.matchMedia('(max-width: 550px)').matches){
+    closeMenu();
+}
+
+function closeMenu(){
+    links.forEach((link) => {
+        link.addEventListener('click', ()=> {
+            linksContainer.classList.remove('active')
+            hamburger.classList.remove('active')
+        })
+    })
 }
